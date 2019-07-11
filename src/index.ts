6177146a3dffb8c77a7ec8c637734efb58d5684e
Carlos Downie <42552189+downiec@users.noreply.github.com>
2019-07-11 16:57:49 -0700

    Preparing extension for publishing on npm.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from "@jupyterlab/application";
 
+import "../style/index.css";
+import { MainMenu, IMainMenu } from "@jupyterlab/mainmenu";
+import {
+  ITutorialManager,
+  TutorialManager
+} from "@downie4/jupyterlab-tutorial";
 
 /**
- * Initialization data for the jupyterlab_tutorial_ext extension.
+ * Initialization data for the jupyterlab_tutorial_manager extension.
  */
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_tutorial_ext',
+const extension: JupyterFrontEndPlugin<ITutorialManager> = {
+  activate,
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_tutorial_ext is activated!');
-  }
+  id: "@cdat/jupyterlab-tutorial-manager",
+  requires: [IMainMenu],
+  provides: ITutorialManager
 };
+
+function activate(app: JupyterFrontEnd, menu: MainMenu): ITutorialManager {
+  // Create tutorial manager
+  const tutorialManager = new TutorialManager(app, menu);
+
+  app.started.then(() => {
+    // Attach the widget to the main work area if it's not there
+    if (!tutorialManager.isAttached) {
+      app.shell.add(tutorialManager, "main");
+    }
+  });
+
+  return tutorialManager;
+}
 
 export default extension;
