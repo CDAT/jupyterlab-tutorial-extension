7a39a540d9b23243fba8f585bb95d864e178e895
Carlos Downie <42552189+downiec@users.noreply.github.com>
2019-08-07 12:31:36 -0700

    Updated the tutorial package.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-} from "@jupyterlab/application";
+} from "@jupyterlab/application"
 
 import "../style/index.css";
 import { MainMenu, IMainMenu } from "@jupyterlab/mainmenu";
-import {
-  ITutorialManager,
-  TutorialManager
-} from "jupyterlab-tutorial";
+import { ITutorialManager, TutorialManager } from "jupyterlab-tutorial";
 
 /**
  * Initialization data for the jupyterlab-tutorial extension.
